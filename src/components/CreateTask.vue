c7c7e2e506d0e5a778ef7f4ee9f74b90e00ecd46
Yogesh Kumar <yogesh.kumar@nagarro.com>
2020-05-21 16:40:59 +0530

    task create popup

--- src/components/CreateTask.vue
+++ src/components/CreateTask.vue
@@ -13,6 +13,7 @@
             name="taskTitle"
             id="taskTitle"
             placeholder="Enter Title"
+            v-model="taskDetail.taskTitle"
             class="bg-gray-400 focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
           />
         </div>
@@ -25,12 +26,69 @@
             name="taskDesc"
             id="taskDesc"
             placeholder="Enter Task Description"
+            v-model="taskDetail.description"
             class="bg-gray-400 focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal resize-none"
           />
         </div>
       </div>
+      <div class="form-fields flex mb-2">
+        <div class="w-1/4 text-sm font-bold">Due Date</div>
+        <div class="w-3/4">
+          <input
+            type="date"
+            name="taskDueDate"
+            id="taskDueDate"
+            v-model="taskDetail.dueDate"
+            class="bg-gray-400 focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal resize-none"
+          />
+        </div>
+      </div>
+      <div class="form-fields flex mb-2">
+        <div class="w-1/4 text-sm font-bold">Category</div>
+        <div class="w-3/4">
+          <select
+            name="taskCat"
+            id="taskCat"
+            v-model="taskDetail.taskCategory"
+            class=" block bg-gray-400 focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal resize-none"
+          >
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          </select>
+        </div>
+      </div>
+      <div class="form-fields flex mb-2">
+        <div class="w-1/4 text-sm font-bold">Priority</div>
+        <div class="w-3/4">
+          <select
+            name="taskPriority"
+            id="taskPriority"
+            v-model="taskDetail.taskPriority"
+            class=" block bg-gray-400 focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal resize-none"
+          >
+          <option>Normal</option>
+          <option>High</option>
+          </select>
+        </div>
+      </div>
+      <div class="form-fields flex mb-2">
+        <div class="w-1/4 text-sm font-bold">Status</div>
+        <div class="w-3/4">
+          <select
+            name="taskStatus"
+            id="taskStatus"
+            v-model="taskDetail.taskStatus"
+            class=" block bg-gray-400 focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal resize-none"
+          >
+          <option>option 1</option>
+          <option>option 2</option>
+          </select>
+        </div>
+      </div>
       <div class="form-actions pt-1 flex justify-end">
-        <button class="bg-blue-500 text-white px-2 py-1 rounded" @click="handlePopupEvent()">Close</button>
+        <button class="bg-blue-500 text-white px-2 py-1 rounded mr-4" @click="handlePopupEvent()">Cancel</button>
+        <button class="bg-blue-500 text-white px-2 py-1 rounded" @click="handlePopupEvent()" name="save">Save</button>
       </div>
     </div>
   </base-dialog>
@@ -51,11 +109,20 @@ export default {
   },
   data() {
     return {
-      width: 700
+      width: 700,
+      taskDetail:{
+        taskTitle:"",
+        description:"",
+        dueDate:new Date().toString().slice(0,10),
+        taskPriority:"",
+        taskCategory:"",
+        taskStatus: "",
+      }
     };
   },
   methods: {
     handlePopupEvent() {
+      //post taskDetail object here
       this.$emit("handlePopupEvent", {
         type: EVENT_CLOSE
       });
